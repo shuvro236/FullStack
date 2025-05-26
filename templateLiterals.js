15212eeb8f76d9f4b61d935b65a6c2a78b36b82d
Shuvro Sarker <71818700+shuvro236@users.noreply.github.com>
2025-05-26 22:23:12 +0600

    Updated template

--- templateLiterals.js
+++ templateLiterals.js
@@ -5,3 +5,12 @@ console.log(multiLineString);
 
 let fristNmae = "Shuvro";
 let lastName = "Sarker";
+
+let greeting =`Hello ${fristNmae} ${lastName}, welcome to the world of JavaScript!`;
+console.log(greeting);
+
+let t =100;
+let y = 20;
+
+let sum = `The sum of ${t} and ${y} is ${t+y}`;
+console.log(sum); // This will not work as expected because of the quotes
