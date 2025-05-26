e345685b39eac2162b7ad9ab97586fbb0fcd8dda
Shuvro Sarker <71818700+shuvro236@users.noreply.github.com>
2025-05-26 17:37:37 +0600

    template

---
+++ templateLiterals.js
@@ -0,0 +1,7 @@
+//let multiLineString = "This is a\n string";// \n is used to create a new line
+let multiLineString = `This is
+ a string`;// ` is used to create a template literal
+console.log(multiLineString);
+
+let fristNmae = "Shuvro";
+let lastName = "Sarker";
