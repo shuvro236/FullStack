0a79e45343649656b8d4d3390f53269357234372
Shuvro Sarker <71818700+shuvro236@users.noreply.github.com>
2025-05-26 16:57:04 +0600

    This is string type

---
+++ primitiveData.js
@@ -0,0 +1,12 @@
+//string
+//anything using quotes is a string
+let myname = "Shuvro";
+let mysername = "Sarker";
+console.log(myname);
+let fullGreeting = myname + " " + mysername;
+console.log(fullGreeting);
+// boolean
+// number
+// null
+// undefined
+// symbol
