1c5d339ffb1e05286f731ac94215d65be9ee6117
Shuvro Sarker <71818700+shuvro236@users.noreply.github.com>
2025-05-26 17:07:19 +0600

    primitiveEnd

--- primitiveData.js
+++ primitiveData.js
@@ -5,8 +5,18 @@ let mysername = "Sarker";
 console.log(myname);
 let fullGreeting = myname + " " + mysername;
 console.log(fullGreeting);
-// boolean
 // number
+
+
+let myage = 27;
+let newAge = myage +1;
+console.log(typeof myage);
+console.log(myage);
+console.log(newAge);
+
+// boolean
+let isStudent = true;
+
 // null
 // undefined
 // symbol
