f781aab820ddeb650b3ce3904370ef3d972fc13e
Shuvro Sarker <71818700+shuvro236@users.noreply.github.com>
2025-06-08 15:46:31 +0600

    new

---
+++ object/object.js
@@ -0,0 +1,10 @@
+let car = {
+    brand : "Toyota",
+    model : "Corolla", 
+    year : 2020,
+};
+
+console.log(car.brand); // Accessing object properties using dot notation
+console.log(car["model"]); // Accessing object properties using bracket notation
+console.log(car.model); // Accessing object properties using dot notation again
+console.log(car.year); // Accessing object properties using bracket notation again
