4547509dcc298148d4fa1b8c70a25f9cc881af39
Shuvro Sarker <71818700+shuvro236@users.noreply.github.com>
2025-06-08 15:29:21 +0600

    yes

---
+++ undefindAndnull.js
@@ -0,0 +1,6 @@
+let undefinedVariable;
+console.log(undefinedVariable); // undefined
+let nullVariable = null;
+console.log("nullVariable:",nullVariable); // null
+console.log("typeof undefinedVariable:", typeof undefinedVariable); // undefined
+console.log("typeof nullVariable:", typeof nullVariable); // object (this is a known JavaScript quirk)
